067616678a86bdf3b089813fda1dfbf4ee05a643
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-12 23:56:06 -0700

    first two requests tested successfully with insomnia

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // implement your API here
+const express = require("express");
+const users = require("./data/db");
+const server = express();
+
+// this is middleware that allows express to parse JSON request bodies
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+ const { name, bio } = req.body;
+
+ if (!name || !bio) {
+  res
+   .status(400)
+   .json({ errorMessage: "Please provide name and bio for the user." });
+ } else {
+  users
+   .insert(req.body)
+   .then(user => {
+    res.status(201).json(user);
+   })
+   .catch(() => {
+    res.status(500).json({
+     errorMessage: "There was an error while saving the user to the database"
+    });
+   });
+ }
+});
+
+server.get("/api/users", (req, res) => {
+ users
+  .find()
+  .then(userlist => {
+   res.status(200).json(userlist);
+  })
+  .catch(() => {
+   res.status(500).json({
+    errorMessage: "The users information could not be retrieved."
+   });
+  });
+});
+
+const port = 5000;
+
+// start the server on localhost at port 5000
+server.listen(port, () => {
+ console.log(`server started at http://localhost:${port})`);
+});
